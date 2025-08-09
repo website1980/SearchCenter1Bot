49cc515a5a1ba57072dafd4151aecd9e7fe121e8
website1980 <ya01875312198@gmail.com>
2025-08-10 01:00:08 +0600

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 # Replace this with your own channel ID
-CHANNEL_ID = -1001955427962
+CHANNEL_ID = -1002616414260
 
 
 @Client.on_message(filters.channel & filters.media)
